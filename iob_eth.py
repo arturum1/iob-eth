991247d99d03886827accc9d8042df482b755620
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-04-02 15:48:58 +0100

    fix(setup): Remove Smart Zynq SL from board list
    IOb_eth core does not yet support building individually for
    the Smart Zynq SL board. But it can be used along with other cores, like
    iob_system.

--- iob_eth.py
+++ iob_eth.py
@@ -70,7 +70,7 @@ def setup(py_params_dict):
         "generate_hw": True,
         "description": "IObundle's ethernet core. Driver-compatible with the OpenCores ethmac core, containing a similar Control/Status Register interface. The original sources for this core and public key signature are available at: https://github.com/IObundle/iob-eth.",
         "version": VERSION,
-        "board_list": ["iob_aes_ku040_db_g", "iob_cyclonev_gt_dk", "iob_smart_zynq_sl"],
+        "board_list": ["iob_aes_ku040_db_g", "iob_cyclonev_gt_dk"],
         "confs": [
             # Macros
             {
